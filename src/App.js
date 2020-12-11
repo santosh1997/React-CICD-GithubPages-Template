df7c56363f804fd7c6c5e343ae2fdea7c5564c77
LsqSantosh <santosh.selvasundar@leadsquared.com>
2020-12-12 01:18:35 +0530

    Plain React App Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="https://github.com/santosh1997/React-CICD-GithubPages-Template">
+          A Simple React CI/CD Github Pages Template
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
