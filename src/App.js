8e3c6aa9ac2ac89f7e1c9fc6955c934bd44a2c05
LsqSantosh <santosh.selvasundar@leadsquared.com>
2020-12-12 02:07:38 +0530

    CI CD Verification

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <a href="https://github.com/santosh1997/React-CICD-GithubPages-Template">
-          A Simple React CI/CD Github Pages Template
+          A Simple React CI/CD Github Pages Template. Please visit!
         </a>
       </header>
     </div>
